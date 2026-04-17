f91c4390f4febb7358a14cc3b2536d53add0033c
Useless8000B <adrielluiz73@gmail.com>
2026-04-17 12:41:45 -0300

    Add new color

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color royalBlue = Color(0xFF5271FF);
   static const Color gray = Color(0xFF9C9C9C);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF0F1011);
+  static const Color red = Colors.redAccent;
   static const Color lightBlack = Color(0xFF1B1B1B);
 }
