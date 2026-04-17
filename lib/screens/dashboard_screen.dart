4322d1dca9d8effff7eab5bf96a9ed84f2ae1260
Useless8000B <adrielluiz73@gmail.com>
2026-04-17 00:16:19 -0300

    Implement storage widget

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,3 +1,4 @@
+import 'package:core_tuner/widgets/storage_widget.dart';
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -5,6 +6,17 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              StorageWidget()
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
