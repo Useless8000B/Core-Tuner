4130e4f552d2266ab83494d1a020982ad0a7b475
Useless8000B <adrielluiz73@gmail.com>
2026-04-17 18:19:04 -0300

    Refactor
    Refactor RamScreen to use StatefulWidget and synchronize ZRAM state on initialization. Updated TweakSwitch to store ZRAM swap preference using SharedPreferences.

--- lib/screens/ram_screen.dart
+++ lib/screens/ram_screen.dart
@@ -1,47 +1,64 @@
 import 'package:core_tuner/services/system_services.dart';
-import 'package:core_tuner/widgets/core_snack_widget.dart';
 import 'package:core_tuner/widgets/ram_widget.dart';
 import 'package:core_tuner/widgets/tweak_switch.dart';
 import 'package:core_tuner/widgets/zram_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class RamScreen extends StatelessWidget {
+class RamScreen extends StatefulWidget {
   const RamScreen({super.key});
+
+  @override
+  State<RamScreen> createState() => _RamScreenState();
+}
+
+class _RamScreenState extends State<RamScreen> {
+  bool isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    _syncSystem();
+  }
+
+  bool _isInitialSync = true;
+
+  Future<void> _syncSystem() async {
+    if (!_isInitialSync) return;
+
+    await SystemService.syncZramState();
+    await Future.delayed(const Duration(milliseconds: 1000));
+
+    if (mounted) {
+      setState(() {
+        isLoading = false;
+        _isInitialSync = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsetsGeometry.all(20),
+          padding: const EdgeInsets.all(20),
           child: Column(
             children: [
-              RamWidget(),
+              const RamWidget(),
               const SizedBox(height: 20),
-              ZramWidget(),
+              const ZramWidget(),
               const SizedBox(height: 20),
               TweakSwitch(
                 title: 'ZRAM Swap',
                 storageKey: 'zram_swap',
-                checkStatus: () => SystemService.isZramActive(),
+                checkStatus: () async {
+                  final prefs = await SharedPreferences.getInstance();
+                  return prefs.getBool('zram_swap') ?? false;
+                },
                 onAction: (value) async {
-                  try {
-                    await SystemService.applyZramTweak(value);
-                    if (context.mounted) {
-                      CoreSnack.show(
-                        context,
-                        "ZRAM Swap ${value ? 'Activated' : 'Deactivated'}",
-                      );
-                    }
-                  } catch (e) {
-                    if (context.mounted) {
-                      CoreSnack.show(
-                        context,
-                        "Error applying: $e",
-                        isError: true,
-                      );
-                    }
-                  }
+                  final prefs = await SharedPreferences.getInstance();
+                  await prefs.setBool('zram_swap', value);
+                  await SystemService.applyZramTweak(value);
                 },
               ),
             ],
