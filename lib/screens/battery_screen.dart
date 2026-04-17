f4334aa30c45ea17c2eac21cf4cdf3decc571698
Useless8000B <adrielluiz73@gmail.com>
2026-04-17 16:00:20 -0300

    Implement new widget

--- lib/screens/battery_screen.dart
+++ lib/screens/battery_screen.dart
@@ -1,3 +1,4 @@
+import 'package:core_tuner/widgets/battery_widget.dart';
 import 'package:flutter/material.dart';
 
 class BatteryScreen extends StatelessWidget {
@@ -5,6 +6,18 @@ class BatteryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              BatteryWidget(),
+              const SizedBox(height: 12,)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
