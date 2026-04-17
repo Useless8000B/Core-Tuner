aeead4aa1bf9faec8ed59ad4d48c95cd7b2c063f
Useless8000B <adrielluiz73@gmail.com>
2026-04-17 12:43:18 -0300

    Implement new tweak widgets

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,8 @@
+import 'package:core_tuner/services/system_services.dart';
+import 'package:core_tuner/widgets/core_snack_widget.dart';
 import 'package:core_tuner/widgets/storage_widget.dart';
+import 'package:core_tuner/widgets/tweak_button.dart';
+import 'package:core_tuner/widgets/tweak_switch.dart';
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -12,7 +16,29 @@ class DashboardScreen extends StatelessWidget {
           padding: EdgeInsets.all(20),
           child: Column(
             children: [
-              StorageWidget()
+              StorageWidget(),
+              const SizedBox(height: 20),
+              TweakButton(
+                title: 'Wipe Dalvik Cache',
+                onAction: SystemService.clearDalvik,
+              ),
+              TweakSwitch(
+                title: 'Wi-Fi Scan Throttling',
+                storageKey: 'wifi_scan_throttling',
+                onAction: (value) async {
+                  try {
+                    await SystemService.setWifiThrottling(value);
+
+                    if (context.mounted) {
+                      CoreSnack.show(context, "Wi-Fi throttling ${value ? 'Activated' : 'Deactivated'}");
+                    }
+                  } catch (e) {
+                    if (context.mounted) {
+                      CoreSnack.show(context, "Error applying: $e", isError: true);
+                    }
+                  }
+                },
+              ),
             ],
           ),
         ),
