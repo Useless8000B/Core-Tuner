0177de5dbd98527a12fc8cc036d8e4b7a790b5ef
Useless8000B <adrielluiz73@gmail.com>
2026-04-19 18:40:17 -0300

    Rename dashboard section to storage

--- lib/navigation/drawer_navigator.dart
+++ lib/navigation/drawer_navigator.dart
@@ -3,18 +3,31 @@ import 'package:flutter/material.dart';
 class DrawerNavigator extends StatelessWidget {
   final int currentIndex;
   final ValueChanged<int> onTap;
-  const DrawerNavigator({super.key, required this.currentIndex, required this.onTap});
+  const DrawerNavigator({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
     return NavigationDrawer(
       selectedIndex: currentIndex,
       onDestinationSelected: onTap,
-      children: [
-        const NavigationDrawerDestination(icon: Icon(Icons.dashboard), label: Text("Dashboard")),
-        const NavigationDrawerDestination(icon: Icon(Icons.memory), label: Text("CPU")),
-        const NavigationDrawerDestination(icon: Icon(Icons.bolt), label: Text("RAM")),
-        const NavigationDrawerDestination(icon: Icon(Icons.battery_charging_full), label: Text("Battery")),
+      children: const [
+        NavigationDrawerDestination(
+          icon: Icon(Icons.storage),
+          label: Text("Storage"),
+        ),
+        NavigationDrawerDestination(
+          icon: Icon(Icons.memory),
+          label: Text("CPU"),
+        ),
+        NavigationDrawerDestination(icon: Icon(Icons.bolt), label: Text("RAM")),
+        NavigationDrawerDestination(
+          icon: Icon(Icons.battery_charging_full),
+          label: Text("Battery"),
+        ),
       ],
     );
   }
