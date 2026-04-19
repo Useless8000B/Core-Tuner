1a48142e851c06b91881e08f27d44e72288473e2
Useless8000B <adrielluiz73@gmail.com>
2026-04-18 22:31:24 -0300

    Add TweakSwitch for Wi-Fi Scan Throttling and Battery Idle Mode in BatteryScreen
    Implemented two TweakSwitch widgets in the BatteryScreen to allow users to manage Wi-Fi Scan Throttling and Battery Idle Mode settings.

--- lib/screens/battery_screen.dart
+++ lib/screens/battery_screen.dart
@@ -1,4 +1,6 @@
+import 'package:core_tuner/services/system_services.dart';
 import 'package:core_tuner/widgets/battery_widget.dart';
+import 'package:core_tuner/widgets/tweak_switch.dart';
 import 'package:flutter/material.dart';
 
 class BatteryScreen extends StatelessWidget {
@@ -13,7 +15,21 @@ class BatteryScreen extends StatelessWidget {
           child: Column(
             children: [
               BatteryWidget(),
-              const SizedBox(height: 12,)
+              const SizedBox(height: 12),
+              TweakSwitch(
+                title: 'Wi-Fi Scan Throttling',
+                storageKey: 'wifi_scan_throttling',
+                onAction: (value) async {
+                  await SystemService.setWifiThrottling(value);
+                },
+              ),
+              TweakSwitch(
+                title: 'Battery Idle Mode',
+                storageKey: 'battery_idle_mode',
+                onAction: (value) async {
+                  await SystemService.setBatteryIdleMode(value);
+                },
+              ),
             ],
           ),
         ),
