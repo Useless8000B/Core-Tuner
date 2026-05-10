8ee1cf497f09f04bedc2da5ee02e762199ef76ac
Useless8000B <adrielluiz73@gmail.com>
2026-05-10 10:50:09 -0300

    Expose function

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -1,9 +1,14 @@
-#[flutter_rust_bridge::frb(sync)]
-pub fn greet(name: String) -> String {
-    format!("Hello, {name}!")
-}
+use crate::system::reader;
+use crate::models::battery_model::BatteryModel;
 
 #[flutter_rust_bridge::frb(init)]
 pub fn init_app() {
     flutter_rust_bridge::setup_default_user_utils();
+}
+
+#[flutter_rust_bridge::frb(sync)]
+pub fn get_battery_info() -> Result<BatteryModel, String> {
+    let battery_info = reader::battery_info()?;
+
+    Ok(battery_info)
 }
