03c1f33104dadfd77d3a59b10d47a7fbf4152800
Useless8000B <adrielluiz73@gmail.com>
2026-05-10 12:13:29 -0300

    Add rust system services

---
+++ lib/services/system_services_rust.dart
@@ -0,0 +1,28 @@
+import 'package:core_tuner/src/rust/api/simple.dart';
+import 'package:core_tuner/src/rust/models/battery_model.dart';
+
+class SystemServicesRust {
+  static Stream<BatteryModel> getBatteryStream() async* {
+    while (true) {
+      try {
+        final info = getBatteryInfo();
+        yield info;
+      } catch (e) {
+        throw Exception("Error reading rust battery: $e");
+      }
+      await Future.delayed(const Duration(seconds: 2));
+    }
+  }
+
+  static Stream<double> getCpuTemperatureStream() async* {
+    while (true) {
+      try {
+        final temp = getCpuTemperature();
+        yield temp.toDouble();
+      } catch (e) {
+        throw Exception("Error reading cpu temperature: $e");
+      }
+      await Future.delayed(const Duration(seconds: 1));
+    }
+  }
+}
