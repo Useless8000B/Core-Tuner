9945f9f90758400baefd169caa6dfd488655f1da
Useless8000B <adrielluiz73@gmail.com>
2026-05-11 00:12:37 -0300

    Update hash

--- rust/src/frb_generated.rs
+++ rust/src/frb_generated.rs
@@ -38,7 +38,7 @@ flutter_rust_bridge::frb_generated_boilerplate!(
     default_rust_auto_opaque = RustAutoOpaqueMoi,
 );
 pub(crate) const FLUTTER_RUST_BRIDGE_CODEGEN_VERSION: &str = "2.12.0";
-pub(crate) const FLUTTER_RUST_BRIDGE_CODEGEN_CONTENT_HASH: i32 = 655817343;
+pub(crate) const FLUTTER_RUST_BRIDGE_CODEGEN_CONTENT_HASH: i32 = 1234460608;
 
 // Section: executor
 
@@ -128,6 +128,35 @@ fn wire__crate__api__simple__get_ram_info_impl(
             deserializer.end();
             transform_result_sse::<_, String>((move || {
                 let output_ok = crate::api::simple::get_ram_info()?;
+                Ok(output_ok)
+            })())
+        },
+    )
+}
+fn wire__crate__api__simple__get_swap_info_impl(
+    ptr_: flutter_rust_bridge::for_generated::PlatformGeneralizedUint8ListPtr,
+    rust_vec_len_: i32,
+    data_len_: i32,
+) -> flutter_rust_bridge::for_generated::WireSyncRust2DartSse {
+    FLUTTER_RUST_BRIDGE_HANDLER.wrap_sync::<flutter_rust_bridge::for_generated::SseCodec, _>(
+        flutter_rust_bridge::for_generated::TaskInfo {
+            debug_name: "get_swap_info",
+            port: None,
+            mode: flutter_rust_bridge::for_generated::FfiCallMode::Sync,
+        },
+        move || {
+            let message = unsafe {
+                flutter_rust_bridge::for_generated::Dart2RustMessageSse::from_wire(
+                    ptr_,
+                    rust_vec_len_,
+                    data_len_,
+                )
+            };
+            let mut deserializer =
+                flutter_rust_bridge::for_generated::SseDeserializer::new(message);
+            deserializer.end();
+            transform_result_sse::<_, String>((move || {
+                let output_ok = crate::api::simple::get_swap_info()?;
                 Ok(output_ok)
             })())
         },
@@ -251,6 +280,22 @@ impl SseDecode for () {
     fn sse_decode(deserializer: &mut flutter_rust_bridge::for_generated::SseDeserializer) -> Self {}
 }
 
+impl SseDecode for crate::models::zram_model::ZramModel {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    fn sse_decode(deserializer: &mut flutter_rust_bridge::for_generated::SseDeserializer) -> Self {
+        let mut var_origin = <f64>::sse_decode(deserializer);
+        let mut var_compressed = <f64>::sse_decode(deserializer);
+        let mut var_total = <f64>::sse_decode(deserializer);
+        let mut var_ratio = <f64>::sse_decode(deserializer);
+        return crate::models::zram_model::ZramModel {
+            origin: var_origin,
+            compressed: var_compressed,
+            total: var_total,
+            ratio: var_ratio,
+        };
+    }
+}
+
 impl SseDecode for i32 {
     // Codec=Sse (Serialization based), see doc to use other codecs
     fn sse_decode(deserializer: &mut flutter_rust_bridge::for_generated::SseDeserializer) -> Self {
@@ -267,7 +312,7 @@ fn pde_ffi_dispatcher_primary_impl(
 ) {
     // Codec=Pde (Serialization + dispatch), see doc to use other codecs
     match func_id {
-        4 => wire__crate__api__simple__init_app_impl(port, ptr, rust_vec_len, data_len),
+        5 => wire__crate__api__simple__init_app_impl(port, ptr, rust_vec_len, data_len),
         _ => unreachable!(),
     }
 }
@@ -283,6 +328,7 @@ fn pde_ffi_dispatcher_sync_impl(
         1 => wire__crate__api__simple__get_battery_info_impl(ptr, rust_vec_len, data_len),
         2 => wire__crate__api__simple__get_cpu_temperature_impl(ptr, rust_vec_len, data_len),
         3 => wire__crate__api__simple__get_ram_info_impl(ptr, rust_vec_len, data_len),
+        4 => wire__crate__api__simple__get_swap_info_impl(ptr, rust_vec_len, data_len),
         _ => unreachable!(),
     }
 }
@@ -330,6 +376,29 @@ impl flutter_rust_bridge::IntoIntoDart<crate::models::ram_model::RamModel>
     for crate::models::ram_model::RamModel
 {
     fn into_into_dart(self) -> crate::models::ram_model::RamModel {
+        self
+    }
+}
+// Codec=Dco (DartCObject based), see doc to use other codecs
+impl flutter_rust_bridge::IntoDart for crate::models::zram_model::ZramModel {
+    fn into_dart(self) -> flutter_rust_bridge::for_generated::DartAbi {
+        [
+            self.origin.into_into_dart().into_dart(),
+            self.compressed.into_into_dart().into_dart(),
+            self.total.into_into_dart().into_dart(),
+            self.ratio.into_into_dart().into_dart(),
+        ]
+        .into_dart()
+    }
+}
+impl flutter_rust_bridge::for_generated::IntoDartExceptPrimitive
+    for crate::models::zram_model::ZramModel
+{
+}
+impl flutter_rust_bridge::IntoIntoDart<crate::models::zram_model::ZramModel>
+    for crate::models::zram_model::ZramModel
+{
+    fn into_into_dart(self) -> crate::models::zram_model::ZramModel {
         self
     }
 }
@@ -400,6 +469,16 @@ impl SseEncode for u8 {
 impl SseEncode for () {
     // Codec=Sse (Serialization based), see doc to use other codecs
     fn sse_encode(self, serializer: &mut flutter_rust_bridge::for_generated::SseSerializer) {}
+}
+
+impl SseEncode for crate::models::zram_model::ZramModel {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    fn sse_encode(self, serializer: &mut flutter_rust_bridge::for_generated::SseSerializer) {
+        <f64>::sse_encode(self.origin, serializer);
+        <f64>::sse_encode(self.compressed, serializer);
+        <f64>::sse_encode(self.total, serializer);
+        <f64>::sse_encode(self.ratio, serializer);
+    }
 }
 
 impl SseEncode for i32 {
