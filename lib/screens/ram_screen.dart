ffd343712a4fe5cee9df1314da0ec17c81379643
Useless8000B <adrielluiz73@gmail.com>
2026-05-15 16:54:48 -0300

    Cleanup

--- lib/screens/ram_screen.dart
+++ lib/screens/ram_screen.dart
@@ -1,10 +1,8 @@
-import 'package:core_tuner/services/system_services.dart';
+import 'package:core_tuner/services/system_services_rust.dart';
 import 'package:core_tuner/widgets/ram_widget.dart';
 import 'package:core_tuner/widgets/tweak_slider.dart';
-import 'package:core_tuner/widgets/tweak_switch.dart';
 import 'package:core_tuner/widgets/zram_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class RamScreen extends StatefulWidget {
   const RamScreen({super.key});
@@ -14,29 +12,6 @@ class RamScreen extends StatefulWidget {
 }
 
 class _RamScreenState extends State<RamScreen> {
-  bool isLoading = true;
-  @override
-  void initState() {
-    super.initState();
-    _syncSystem();
-  }
-
-  bool _isInitialSync = true;
-
-  Future<void> _syncSystem() async {
-    if (!_isInitialSync) return;
-
-    await SystemService.syncZramState();
-    await Future.delayed(const Duration(milliseconds: 1000));
-
-    if (mounted) {
-      setState(() {
-        isLoading = false;
-        _isInitialSync = false;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,27 +24,13 @@ class _RamScreenState extends State<RamScreen> {
               const SizedBox(height: 20),
               const ZramWidget(),
               const SizedBox(height: 20),
-              TweakSwitch(
-                title: 'ZRAM Swap',
-                storageKey: 'zram_swap',
-                checkStatus: () async {
-                  final prefs = await SharedPreferences.getInstance();
-                  return prefs.getBool('zram_swap') ?? false;
-                },
-                onAction: (value) async {
-                  final prefs = await SharedPreferences.getInstance();
-                  await prefs.setBool('zram_swap', value);
-                  // TODO Implement logic
-                },
-              ),
-              const SizedBox(height: 20),
               TweakSlider(
                 title: 'Swappiness',
                 storageKey: 'swappiness',
-                labelLeft: 'performance',
+                labelLeft: 'Performance',
                 labelRight: 'Multitasking',
-                onAction: (value) {
-                  // TODO Implement logic
+                onAction: (value) async {
+                  await SystemServicesRust.applySwappiness(value);
                 },
               ),
               TweakSlider(
