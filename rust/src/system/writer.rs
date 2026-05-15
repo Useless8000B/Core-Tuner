31b3232a20a6eb5b41f0591db5abb9a4cddb8972
Useless8000B <adrielluiz73@gmail.com>
2026-05-15 17:26:35 -0300

    Add new method
    New method to set swappiness

--- rust/src/system/writer.rs
+++ rust/src/system/writer.rs
@@ -5,7 +5,8 @@ use crate::system::properties::Property;
 
 pub fn set_cpu_governor(choice: &str) -> Result<(), String> {
     let properties = Property::cpu_cores_properties();
-    let gov = Governor::from_input(choice).ok_or("Error reading from input")?;
+    let gov = Governor::from_input(choice)
+        .ok_or("Error reading from input")?;
 
     let entry = properties
         .iter()
@@ -23,6 +24,34 @@ pub fn set_cpu_governor(choice: &str) -> Result<(), String> {
         .arg(&cmd)
         .status()
         .map_err(|e| format!("Failed to execute su: {e}"))?;
+
+    if status.success() {
+        Ok(())
+    } else {
+        Err(format!("Command failed due to an error: {status}"))
+    }
+}
+
+pub fn set_swappiness(choice: u8) -> Result<(), String> {
+    let properties = Property::kernel_properties();
+    let entry = properties
+        .iter()
+        .find(|v| v.name == "swappiness")
+        .ok_or("swappiness property not found")?;
+
+    let safe_value = choice.clamp(0, 100);
+
+    let cmd = format!(
+        "echo {} > {}",
+        safe_value.to_string(),
+        entry.path
+    );
+
+    let status = Command::new("su")
+        .arg("-c")
+        .arg(&cmd)
+        .status()
+        .map_err(|e| format!("Error appling swappiness: {e}"))?;
 
     if status.success() {
         Ok(())
