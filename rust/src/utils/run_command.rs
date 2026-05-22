cb0822fbb16223f73151969c4bd822f986cb08fa
Useless8000B <adrielluiz73@gmail.com>
2026-05-22 11:45:19 -0300

    Undo rename and add new method
    New method executes fstrim

--- rust/src/utils/run_command.rs
+++ rust/src/utils/run_command.rs
@@ -1,6 +1,6 @@
 use std::process::Command;
 
-pub fn run_command(cmd: &str, args: &[&str]) -> Result<(), String> {
+pub fn write_shell_command(cmd: &str, args: &[&str]) -> Result<(), String> {
 	let status = Command::new(cmd)
 		.args(args)
 		.status()
