3d27c4189c6001d8a0a53fc15ee790b1bb401374
Useless8000B <adrielluiz73@gmail.com>
2026-08-19 20:29:29 -0300

    Replace ok_or with ok_or_else in write.rs

--- rust/src/system/writer.rs
+++ rust/src/system/writer.rs
@@ -10,7 +10,7 @@ pub fn set_cpu_governor(choice: &str) -> Result<(), String> {
     let entry = properties
         .iter()
         .find(|v| v.name == "cpu_core")
-        .ok_or("cpu_core property not found")?;
+        .ok_or_else(|| "cpu_core property not found")?;
 
     let cmd = format!("printf %s \"{}\" | tee {}", gov.as_string(), entry.path);
 
