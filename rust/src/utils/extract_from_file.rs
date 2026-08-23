4fd80664383995f038b03a4022fed5389c399fe5
Useless8000B <adrielluiz73@gmail.com>
2026-08-23 13:41:49 -0300

    Add lifetimes support

--- rust/src/utils/extract_from_file.rs
+++ rust/src/utils/extract_from_file.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 use crate::errors::reader_error::ReaderError;
 
-pub fn extract_from_label(path: &str, label: &str) -> Result<f64, ReaderError> {
+pub fn extract_from_label(path: &'static str, label: &'static str) -> Result<f64, ReaderError> {
     let content =
         fs::read_to_string(path)
             .map_err(|_| ReaderError::ReadingError("Error reading file".to_string()))?;
@@ -23,15 +23,15 @@ pub fn extract_from_label(path: &str, label: &str) -> Result<f64, ReaderError> {
     Ok(memory_value)
 }
 
-pub fn extract_from_index(path: &str, index: usize) -> Result<f64, ReaderError> {
+pub fn extract_from_index(path: &'static str, index: usize) -> Result<f64, ReaderError> {
     let content = fs::read_to_string(path)
-        .map_err(|_| ReaderError::ReadingError(path.to_string()))?;
+        .map_err(|_| ReaderError::ReadingError(format!("Error reading: {:?}", path)))?;
 
     let content = content
         .split_whitespace()
         .nth(index)
         .and_then(|value| value.parse::<f64>().ok())
-        .ok_or_else(|| ReaderError::ReadingError("Error reading content".to_string()))?;
+        .ok_or_else(|| ReaderError::ReadingError(format!("Error reading {content}")))?;
 
     Ok(content)
 }
