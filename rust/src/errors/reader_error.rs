f51a4d70356009410839671bb6c96e066ead76e9
Useless8000B <adrielluiz73@gmail.com>
2026-08-23 13:25:11 -0300

    Implement traits: Display and From

--- rust/src/errors/reader_error.rs
+++ rust/src/errors/reader_error.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 pub enum ReaderError {
     FileNotFound(String),
     SensorNotFound(String),
@@ -5,4 +7,23 @@ pub enum ReaderError {
     InvalidValue(String),
     ReadingError(String),
     Io(std::io::Error),
+}
+
+impl fmt::Display for ReaderError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ReaderError::FileNotFound(e) => write!(f, "File not found: {e}"),
+            ReaderError::SensorNotFound(e) => write!(f, "Sensor not found: {e}"),
+            ReaderError::PropertyNotFound(e) => write!(f, "Property not found: {e}"),
+            ReaderError::InvalidValue(e) => write!(f, "Invalid value: {e}"),
+            ReaderError::ReadingError(e) => write!(f, "Reading error: {e}"),
+            ReaderError::Io(e) => write!(f, "Io error: {e}")
+        }
+    }
+}
+
+impl From<ReaderError> for String {
+    fn from(error: ReaderError) -> Self {
+        error.to_string()
+    }
 }
