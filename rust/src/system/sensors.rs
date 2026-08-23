ad8540845526fa2dd1fa9f612f12eba393bdeff7
Useless8000B <adrielluiz73@gmail.com>
2026-08-23 13:40:48 -0300

    Refactor
    - Implement lifetimes to decrease heap allocations
    - Remove vec![] to decrease heap allocations
    - Replace clone implementations with format

--- rust/src/system/sensors.rs
+++ rust/src/system/sensors.rs
@@ -1,44 +1,53 @@
 use std::fs;
 
-use crate::{errors::reader_error::ReaderError};
+use crate::errors::reader_error::ReaderError;
 
 pub struct Sensor {
-    pub name: String,
-    path: String,
+    pub name: &'static str,
+    path: &'static str,
 }
 
 impl Sensor {
-    pub fn new(name: &str, path: &str) -> Self {
-        Sensor {
-            name: name.to_string(),
-            path: path.to_string(),
-        }
+    pub const fn new(name: &'static str, path: &'static str) -> Self {
+        Self { name, path }
     }
 
-    pub fn battery_sensors() -> Vec<Sensor> {
-        vec![
+    pub fn battery_sensors() -> &'static [Sensor] {
+        const BATTERY_SENSORS: &[Sensor] = &[
             Sensor::new("temperature", "/sys/class/thermal/thermal_zone40/temp"),
             Sensor::new("voltage", "/sys/class/power_supply/battery/voltage_now"),
             Sensor::new("current", "/sys/class/power_supply/battery/current_now"),
-        ]
+        ];
+
+        BATTERY_SENSORS
     }
 
-    pub fn cpu_sensors() -> Vec<Sensor> {
-        vec![Sensor::new(
+    pub fn cpu_sensors() -> &'static [Sensor] {
+        const CPU_SENSORS: &[Sensor] = &[Sensor::new(
             "performance_core",
             "/sys/class/thermal/thermal_zone7/temp",
-        )]
+        )];
+
+        CPU_SENSORS
     }
 
     pub fn read_sensor(&self) -> Result<f64, ReaderError> {
         let raw_content = match fs::read_to_string(&self.path) {
             Ok(c) => c,
-            Err(_) => return Err(ReaderError::ReadingError(self.name.clone())),
+            Err(_) => {
+                return Err(ReaderError::ReadingError(format!(
+                    "Error reading sensor {}",
+                    self.name
+                )))
+            }
         };
 
         match raw_content.trim().parse::<f64>() {
             Ok(c) => Ok(c),
-            Err(_) => Err(ReaderError::InvalidValue(self.name.clone())),
+            Err(_) => Err(ReaderError::InvalidValue(format!(
+                "Invalid value from sensor {}",
+                self.name
+            ))),
         }
     }
 
